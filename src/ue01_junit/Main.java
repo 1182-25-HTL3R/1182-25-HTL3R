cc24a74e3b64520af1ecb9d43e3025ae60d21e6d
Fabian Ha, 4CN <1182@htl.rennweg.at>
2024-10-15 14:31:45 +0200

    Verbesserung der count Methode, nach der Erkennung, dass es alt-Texte gibt

--- src/ue01_junit/Main.java
+++ src/ue01_junit/Main.java
@@ -1,17 +1,15 @@
 package ue01_junit;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(count(("")));
-    }
+import java.util.Arrays;
 
+public class Main {
     public static int count(String s) {
-        s = s.replaceAll("<[^>]*>", " ");
+        s = s.replaceAll("<((\".*\")|([^>\"]*))*>", " ");
+        s = s.replaceAll("<.*", " ");
         s = s.replaceAll("[^a-zA-Z]", " ");
         s = s.trim().replaceAll(" +", " ");
 
         if (s.isEmpty()) return 0;
-
         return s.split(" ").length;
     }
 }
