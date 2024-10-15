97f0b8e978fcc0142f7fe8844bf4eb6adf85855e
Fabian Ha, 4CN <1182@htl.rennweg.at>
2024-10-15 13:25:30 +0200

    Fehler im Falle eines leeren Strings behoben

--- src/ue01_junit/Main.java
+++ src/ue01_junit/Main.java
@@ -1,18 +1,17 @@
 package ue01_junit;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(count((" \\\"null\\\" one<img alt=\"<bild \\\" keinwort keinwort\" keinwort>two \"three\"")));
+        System.out.println(count(("")));
     }
 
     public static int count(String s) {
         s = s.replaceAll("<[^>]*>", " ");
         s = s.replaceAll("[^a-zA-Z]", " ");
         s = s.trim().replaceAll(" +", " ");
-        String[] splitted = s.split(" ");
-        System.out.println(Arrays.toString(splitted));
-        return splitted.length;
+
+        if (s.isEmpty()) return 0;
+
+        return s.split(" ").length;
     }
 }
