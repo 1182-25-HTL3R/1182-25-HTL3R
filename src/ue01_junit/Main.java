79c06307c9a78b58c1616642e52e18a6e658269a
Fabian Ha, 4CN <1182@htl.rennweg.at>
2024-11-11 17:10:27 +0100

    Ergänzung der Java-Doc

--- src/ue01_junit/Main.java
+++ src/ue01_junit/Main.java
@@ -1,12 +1,16 @@
 package ue01_junit;
 
 public class Main {
+    /**
+     * Methode zum Zählen der Worte
+     * @param s Input-String
+     * @return Anzahl der Worte
+     */
     public static int count(String s) {
         s = s.replaceAll("<((\".*\")|([^>\"]*))*>", " ");
         s = s.replaceAll("<.*", " ");
         s = s.replaceAll("[^a-zA-Z]", " ");
         s = s.trim().replaceAll(" +", " ");
-
         if (s.isEmpty()) return 0;
         return s.split(" ").length;
     }
