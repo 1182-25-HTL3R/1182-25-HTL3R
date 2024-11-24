d54aea2ac1e848eb434650156a8b8daa829117c6
Fabian Ha, 4CN <1182@htl.rennweg.at>
2024-11-24 20:08:47 +0100

    Erstellen eines Unit-Tests für die count Methode von WordCount

---
+++ ue02/src/enum1/TestWordCount.java
@@ -0,0 +1,14 @@
+package enum1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TestWordCount {
+    @org.junit.jupiter.api.Test
+    public void testCount() throws IOException {
+        WordCount wc = new WordCount();
+        wc.count(String.valueOf(Files.readAllLines(Path.of("C:\\Users\\fabia\\IdeaProjects\\SEW4_SEM1\\ue02\\src\\enum1\\crsto12.html"))));
+        assert wc.counter == 48125; // Mitschüler gefragt nach der Anzahl
+    }
+}
