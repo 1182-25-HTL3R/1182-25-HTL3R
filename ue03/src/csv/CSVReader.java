90edb893e7e221114018ea7b26ea06c315f24752
Fabian Ha, 4CN <1182@htl.rennweg.at>
2024-12-05 11:27:54 +0100

    ue03_csv: init und erste Version der Finite State Machine für den CSVReader

---
+++ ue03/src/csv/CSVReader.java
@@ -0,0 +1,20 @@
+/**
+ * @author Fabian Ha
+ * class: 4CN
+ * date: 05.12.2024
+ * program: CSVReader
+ */
+
+package csv;
+
+import java.util.ArrayList;
+
+public class CSVReader {
+
+    public static void main(String[] args) {
+        ArrayList<String> abc = new ArrayList<>();
+        abc.add("bfesaf");
+        abc.toArray();
+
+    }
+}
