8b45caa33aa1f19b197586f354016cd042273794
Fabian Ha, 4CN <1182@htl.rennweg.at>
2024-12-08 21:18:54 +0100

    ue03_TestCSVReader: Erstellung des Testfalls Adjazenzmatrix

--- ue03/src/csv/TestCSVReader.java
+++ ue03/src/csv/TestCSVReader.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class TestCSVReader {
     @Test
@@ -40,12 +41,30 @@ public class TestCSVReader {
 
         // B6
         try (
-            CSVFileReader reader3 = new CSVFileReader("C:\\Users\\fabia\\IdeaProjects\\SEW4_SEM1\\ue03\\src\\csv\\file.csv", ';', '\"', true)) {
+                CSVFileReader reader3 = new CSVFileReader("C:\\Users\\fabia\\IdeaProjects\\SEW4_SEM1\\ue03\\src\\csv\\file.csv", ';', '\"', true)) {
             assert Arrays.equals(reader3.next(), new String[]{"", "A", "B", "C", "D", "E", "F", "G", "H"});
             assert Arrays.equals(reader3.next(), new String[]{"A", "", "4", "7", "8", "", "", "", ""});
             assert Arrays.equals(reader3.next(), new String[]{"B", "4", "", "", "", "5", "3", "", ""});
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (
+                CSVFileReader reader = new CSVFileReader("C:\\Users\\fabia\\IdeaProjects\\SEW4_SEM1\\ue03\\src\\csv\\file.csv", ';', '\"', true)) {
+            String[] firstRow = reader.next();
+            for (String[] row : reader) {
+                System.out.print(row[0]);
+                String s = ":";
+                for (int i = 1; i < row.length; i++) {
+                    if (!Objects.equals(row[i], "")) {
+                        s += " nach " + firstRow[i] + ":" + row[i] + ",";
+                    }
+                }
+                s = s.substring(0, s.length() - 1);
+                System.out.println(s);
+            }
         }
     }
 }
