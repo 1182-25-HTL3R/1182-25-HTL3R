2647efe936c43db39e387a9c44fbb8dad7f1c70f
Fabian Ha, 4CN <1182@htl.rennweg.at>
2024-12-08 16:19:59 +0100

    ue03_TestCSVReader: Hinzufügen der JUnit Tests für B2 und Verbessern einer Kleinigkeit in CSVReader

--- ue03/src/csv/TestCSVReader.java
+++ ue03/src/csv/TestCSVReader.java
@@ -1,5 +1,6 @@
 package csv;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -7,7 +8,19 @@ import java.util.Arrays;
 public class TestCSVReader {
     @Test
     public void testGetWords() {
+        // B1
         assert Arrays.equals(CSVReader.getWords("one,two,three"), new String[]{"one", "two", "three"});
         assert Arrays.equals(CSVReader.getWords("abc,,def123+#+sdaf,ghi"), new String[]{"abc", "", "def123+#+sdaf", "ghi"});
+
+        // B2
+        assert Arrays.equals(CSVReader.getWords("\"uno\",dos,\"tres, cuatro\",cin\"co\""), new String[]{"uno", "dos", "tres, cuatro", "cinco"});
+        IllegalArgumentException excp = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CSVReader.getWords("\"hall\"o");
+        });
+
+        excp = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CSVReader.getWords("\"hallo");
+        });
+
     }
 }
