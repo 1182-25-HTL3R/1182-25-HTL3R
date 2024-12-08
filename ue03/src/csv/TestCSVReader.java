14aa0cd4fb66979af77fde8e3d2fb21f0f9d15a8
Fabian Ha, 4CN <1182@htl.rennweg.at>
2024-12-08 19:36:14 +0100

    ue03_TestCSVReader: Hinzufügen der Testcases für B5

--- ue03/src/csv/TestCSVReader.java
+++ ue03/src/csv/TestCSVReader.java
@@ -8,26 +8,31 @@ import java.util.Arrays;
 public class TestCSVReader {
     @Test
     public void testGetWords() {
+        CSVReader reader = new CSVReader(',', '"', true);
         // B1
-        assert Arrays.equals(CSVReader.getWords("one,two,three"), new String[]{"one", "two", "three"});
-        assert Arrays.equals(CSVReader.getWords("abc,,def123+#+sdaf,ghi"), new String[]{"abc", "", "def123+#+sdaf", "ghi"});
+        assert Arrays.equals(reader.getWords("one,two,three"), new String[]{"one", "two", "three"});
+        assert Arrays.equals(reader.getWords("abc,,def123+#+sdaf,ghi"), new String[]{"abc", "", "def123+#+sdaf", "ghi"});
 
         // B2
-        assert Arrays.equals(CSVReader.getWords("\"uno\",dos,\"tres, cuatro\",cin\"co\""), new String[]{"uno", "dos", "tres, cuatro", "cinco"});
+        assert Arrays.equals(reader.getWords("\"uno\",dos,\"tres, cuatro\",cin\"co\""), new String[]{"uno", "dos", "tres, cuatro", "cinco"});
         IllegalArgumentException excp = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            CSVReader.getWords("\"hall\"o");
+            reader.getWords("\"hall\"o");
         });
 
         excp = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            CSVReader.getWords("\"hallo");
+            reader.getWords("\"hallo");
         });
 
         // B3
-        assert Arrays.equals(CSVReader.getWords("\"ein,s\",\"zw\"\"ei\",drei"), new String[]{"ein,s", "zw\"ei", "drei"});
-        assert Arrays.equals(CSVReader.getWords("\"\"\"ein,s\"\"\""), new String[]{"\"ein,s\""});
+        assert Arrays.equals(reader.getWords("\"ein,s\",\"zw\"\"ei\",drei"), new String[]{"ein,s", "zw\"ei", "drei"});
+        assert Arrays.equals(reader.getWords("\"\"\"ein,s\"\"\""), new String[]{"\"ein,s\""});
 
         // B4
-        assert Arrays.equals(CSVReader.getWords("    \"ab,cd\""), new String[]{"ab,cd"});
-        assert Arrays.equals(CSVReader.getWords("    \"ab,cd\",     efg"), new String[]{"ab,cd", "efg"});
+        assert Arrays.equals(reader.getWords("    \"ab,cd\""), new String[]{"ab,cd"});
+        assert Arrays.equals(reader.getWords("    \"ab,cd\",     efg"), new String[]{"ab,cd", "efg"});
+
+        CSVReader reader2 = new CSVReader('#', '+', false);
+        assert Arrays.equals(reader2.getWords("   one#two#t+hree+"), new String[]{"   one", "two", "three"});
+        assert Arrays.equals(reader2.getWords("abc##+def123sdaf+#ghi"), new String[]{"abc", "", "def123sdaf", "ghi"});
     }
 }
