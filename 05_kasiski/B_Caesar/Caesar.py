46e0f37967dd0635fef5e1e1b14ab54d7c69fe6f
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-02-22 15:11:53 +0100

    UE05_B: Hinzufügen von Doctests und Methode statisch deklarieren

--- 05_kasiski/B_Caesar/Caesar.py
+++ 05_kasiski/B_Caesar/Caesar.py
@@ -1,21 +1,28 @@
 import re, doctest
 
-class Caesar():
+
+class Caesar:
     key: str
+
     def __init__(self, key=None):
         self.key = key
 
-
-    def to_lowercase_letter_only(self, plaintext:str)-> str:
+    @staticmethod
+    def to_lowercase_letter_only(plaintext: str) -> str:
         """Wandelt den plaintext in Kleinbuchstaben um und entfernt alle Zeichen, die keine
         Kleinbuchstaben aus dem Bereich [a..z] sind.
         >>> caesar = Caesar()
         >>> caesar.to_lowercase_letter_only("Wandelt den plaintext in Kleinbuchstaben um und entfernt alle Zeichen, die keine Kleinbuchstaben aus dem Bereich [a..z] sind.")
         'wandeltdenplaintextinkleinbuchstabenumundentferntallezeichendiekeinekleinbuchstabenausdembereichazsind'
+        >>> caesar.to_lowercase_letter_only("Test 123 ! HaLLO")
+        'testhallo'
+        >>> caesar.to_lowercase_letter_only("#+f329jfd0sjdfj9032")
+        'fjfdsjdfj'
         """
 
         plaintext = plaintext.lower()
         return re.compile('[^a-z]').sub('', plaintext)
+
 
 if __name__ == '__main__':
     doctest.testmod(verbose=True)
