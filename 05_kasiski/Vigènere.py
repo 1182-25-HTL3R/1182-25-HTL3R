76f48bde4865fc0bfb08bec2c1e5b74f47928e3b
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-02-22 17:55:02 +0100

    UE05_C: Erstellen der Funktion decrypt mit doctests

--- 05_kasiski/Vigènere.py
+++ 05_kasiski/Vigènere.py
@@ -1,6 +1,7 @@
 from Caesar import Caesar
 import doctest
 
+
 class Vigenere:
     key: str
 
@@ -45,6 +46,45 @@ class Vigenere:
             key_index += 1
 
         return encrypted
+
+    def decrypt(self, crypttext: str, key: str = None) -> str:
+        """
+        entschlüsselt den plaintext mit der Vigènere-Chiffre mit key als Schlüssel
+
+        :param crypttext: verschlüsselter Text
+        :param key: key ist ein String, der definiert, um wieviele Zeichen verschoben wird. Falls kein key übergeben wird, nimmt übernimmt decrypt den Wert vom Property.
+        :return: entschlüsselten Text
+
+        >>> vigenere = Vigenere("#+!x")
+        >>> vigenere.decrypt("xyz")
+        'abc'
+        >>> vigenere.decrypt("abc", "uiop")
+        'gto'
+        >>> vigenere.decrypt("hblmo", "ABABA")
+        'hallo'
+        """
+
+        if key is None:
+            key = self.key
+        key = Caesar.to_lowercase_letter_only(key)
+        if key == "":
+            raise Exception("invalid key")
+
+        crypttext = Caesar.to_lowercase_letter_only(crypttext)
+
+        key_index = 0
+        decrypted = ""
+
+        for i in crypttext:
+            if key_index >= key.__len__():
+                key_index = 0
+
+            caesar = Caesar()
+            decrypted += caesar.decrypt(i, key[key_index])
+
+            key_index += 1
+
+        return decrypted
 
 
 if __name__ == '__main__':
