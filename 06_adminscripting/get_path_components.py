d031b4f880cb1fcae3a81b9619f671bc2017e5e9
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-03-09 13:34:17 +0100

    UE06_A.2: Kommentar bei Übung 1 hinzugefügt

--- 06_adminscripting/get_path_components.py
+++ 06_adminscripting/get_path_components.py
@@ -4,12 +4,17 @@ Program UE06_Adminscripting - pathlib-Modul
 """
 __author__ = "Fabian Ha"
 
+"""
+Liest einen absoluten Pfad ein und gibt die in der Angabe definierten Verzeichnisteile aus
+"""
+
 from pathlib import Path
+
 p = Path(input("Absoluten Pfad einer Datei angeben!"))
-print(f"Dateiname: {p.name}\nDateiname ohne Dateierweiterung: {p.stem}\nDateierweiterung: {p.suffix}\nTeil des Pfades vor den Verzeichnissen: {p.anchor}\nParent Verzeichnis: {p.parent}")
+print(
+    f"Dateiname: {p.name}\nDateiname ohne Dateierweiterung: {p.stem}\nDateierweiterung: {p.suffix}\nTeil des Pfades vor den Verzeichnissen: {p.anchor}\nParent Verzeichnis: {p.parent}")
 
 if p.parent.parent:
     print(p.parent.parent)
 else:
     print("Parent directory does not exist!")
-
