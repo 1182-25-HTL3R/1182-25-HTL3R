dcb4631eca3ffc4e63c77640d6c92203fa78bd3e
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-03-09 13:18:10 +0100

    UE06_A.2: Erstellung von Übung 1

--- 06_adminscripting/get_path_components.py
+++ 06_adminscripting/get_path_components.py
@@ -3,3 +3,13 @@ Class: 4CN
 Program UE06_Adminscripting - pathlib-Modul
 """
 __author__ = "Fabian Ha"
+
+from pathlib import Path
+p = Path(input("Absoluten Pfad einer Datei angeben!"))
+print(f"Dateiname: {p.name}\nDateiname ohne Dateierweiterung: {p.stem}\nDateierweiterung: {p.suffix}\nTeil des Pfades vor den Verzeichnissen: {p.anchor}\nParent Verzeichnis: {p.parent}")
+
+if p.parent.parent:
+    print(p.parent.parent)
+else:
+    print("Parent directory does not exist!")
+
