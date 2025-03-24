60917ea67eb4496c1f74ea806dd74d0768136a06
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-03-24 10:42:27 +0100

    UE07_cvcrack: Schreiben der weiteren Behandlung der args zum Cracken

--- 07_py_argparse/cvcrack.py
+++ 07_py_argparse/cvcrack.py
@@ -1,6 +1,9 @@
 __author__ = "Fabian Ha"
 
 import argparse
+import sys, os
+from Caesar import Caesar
+from Kasiski import Kasiski
 
 def main() -> None:
     """
@@ -24,3 +27,31 @@ def main() -> None:
     parser.add_argument(
         "-q", "--quiet", action="store_true", help="Keine Ausgabe"
     )
+
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.infile):
+        print(f"{args.infile}: No such file or directory", file=sys.stderr)
+        sys.exit(1)
+
+    with open(args.infile, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    chiffre_typ = ""
+    key = ""
+    if args.cipher in ["caesar", "c"]:
+        chiffre_typ = "Caesar"
+        cipher = Caesar()
+        key = cipher.crack(text, 1)[0]
+    elif args.cipher in ["vigenere", "v"]:
+        chiffre_typ = "Vigenere"
+        cipher = Kasiski(text)
+        key = cipher.crack_key(6)
+
+    if args.verbose:
+        print(f"Cracking {chiffre_typ}-encrypted file {args.infile}: Key = {key}")
+    else:
+        print(key)
+
+if __name__ == "__main__":
+    main()
