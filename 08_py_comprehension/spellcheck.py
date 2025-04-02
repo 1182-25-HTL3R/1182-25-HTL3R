5c042ea8489218de11258a07dfea48f0f0db31e9
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-04-02 20:43:58 +0200

    UE08_B3: Erstellung der Funktion edit1 und Schreiben der PyDoc für edit1

--- 08_py_comprehension/spellcheck.py
+++ 08_py_comprehension/spellcheck.py
@@ -21,6 +21,29 @@ def split_word(wort: str) -> list[tuple[str, str]]:
     return [(wort[:x], wort[x:]) for x in range(len(wort) + 1)]
 
 
+def edit1(wort: str) -> set[str]:
+    """
+    bestimmt mit dem Ergebnis von split_word() alle möglichen Wörter mit einem Fehler
+    a) ein Buchstabe fehlt
+    b) zwei Buchstaben verdreht sind
+    c) ein Buchstabe durch einen anderen Buchstaben ersetzt wurde
+    d) ein Buchstabe eingefügt wurde
+    :param wort: Wort welches auf Fehler angesehen wird
+    :return: Set aus möglichen Wörtern
+    """
+    li = split_word(wort)
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    # print({x[0] + x[1][1:] for x in li})  # a)
+    # print({x[0] + x[1][1] + x[1][0] + x[1][2:] for x in li if len(x[1]) >= 2})  # b)
+    # print({x[0] + y + x[1][1:] for x in li for y in alphabet})    # c)
+    # print({x[0] + y + x[1] for x in li for y in alphabet})    # d)
+    return ({x[0] + x[1][1:] for x in li}
+            .union({x[0] + x[1][1] + x[1][0] + x[1][2:] for x in li if len(x[1]) >= 2})
+            .union({x[0] + y + x[1][1:] for x in li for y in alphabet})
+            .union({x[0] + y + x[1] for x in li for y in alphabet}))
+
+
 if __name__ == '__main__':
     print(read_all_words('C:\\Users\\fabia\\Downloads\\08_py_comprehension\\de-en\\de-en.txt'))
     print(split_word("abc"))
+    print(edit1("abcd"))
