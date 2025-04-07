12dd0ae2371ee020eb12f2dfd926acc13c97e0f7
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-04-07 20:03:32 +0200

    UE09: Verbesserung der __init__ Funktion wegen dem falschen Datentyp (float) und __add__ Funktion

--- 09_py_fraction/fraction.py
+++ 09_py_fraction/fraction.py
@@ -1,7 +1,19 @@
+import math
+
+
 class Fraction:
     def __init__(self, numerator: int, denominator: int):
-        self.numerator = numerator
-        self.denominator = denominator
+        gcd = math.gcd(numerator, denominator)
+
+        if denominator > 0:
+            self.numerator = int(numerator / gcd)
+            self.denominator = int(denominator / gcd)
+        else:
+            if denominator == 0:
+                raise ZeroDivisionError
+
+            self.numerator = int(-numerator / gcd)
+            self.denominator = int(-denominator / gcd)
 
     def __str__(self):
         if self.denominator == 1:
@@ -10,5 +22,13 @@ class Fraction:
         return str(self.numerator) + "/" + str(self.denominator)
 
     def __add__(self, other):
-        return Fraction(self.numerator * other.denominator + self.denominator * other.nominator,
+        return Fraction(self.numerator * other.denominator + self.denominator * other.numerator,
                         self.denominator * other.denominator)
+
+
+if __name__ == "__main__":
+    f1 = Fraction(1, 2)
+    f2 = Fraction(2, 3)
+    print(f1)
+    print(f2)
+    print(f1 + f2)
