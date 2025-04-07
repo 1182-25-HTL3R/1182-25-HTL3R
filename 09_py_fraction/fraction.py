4e72f7cfa8caf241c82ed6ffb7a2a0c374182c9a
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-04-07 19:40:44 +0200

    UE09: Überarbeitung der __str__ Funktion zur richtigen Darstellung der Brüche

--- 09_py_fraction/fraction.py
+++ 09_py_fraction/fraction.py
@@ -2,3 +2,9 @@ class Fraction:
     def __init__(self, numerator: int, denominator: int):
         self.numerator = numerator
         self.denominator = denominator
+
+    def __str__(self):
+        if self.denominator == 1:
+            return str(self.numerator)
+
+        return str(self.numerator) + "/" + str(self.denominator)
