8b79452c7a4975e8513bc70b54d8091854e6e86e
1182-25-HTL3R <135468563+1182-25-HTL3R@users.noreply.github.com>
2025-04-07 20:19:42 +0200

    UE09: Schreiben der PyDoc

--- 09_py_fraction/fraction.py
+++ 09_py_fraction/fraction.py
@@ -2,7 +2,16 @@ import math
 
 
 class Fraction:
+    """
+    Representiert einen Bruch mit Nenner (numerator) und Zähler (denominator)
+    """
+
     def __init__(self, numerator: int, denominator: int):
+        """
+        Konstruktor für einen Bruch
+        :param numerator: Nenner
+        :param denominator: Zähler
+        """
         gcd = math.gcd(numerator, denominator)
 
         if denominator > 0:
@@ -16,29 +25,53 @@ class Fraction:
             self.denominator = int(-denominator / gcd)
 
     def __str__(self):
+        """
+        erstellt eine sinnvolle Representation eines Bruchs
+        :return: String Representation eines Bruchs
+        """
         if self.denominator == 1:
             return str(self.numerator)
 
         return str(self.numerator) + "/" + str(self.denominator)
 
     def __add__(self, other):
+        """
+        addiert zwei Brüche
+        :param other: anderer Bruch
+        :return: addierter Bruch
+        """
         return Fraction(self.numerator * other.denominator + self.denominator * other.numerator,
                         self.denominator * other.denominator)
 
     def __sub__(self, other):
+        """
+        subtrahiert zwei Brüche
+        :param other: anderer Bruch
+        :return: subtrahierter Bruch
+        """
         return Fraction(self.numerator * other.denominator - self.denominator * other.numerator,
                         self.denominator * other.denominator)
 
     def __mul__(self, other):
+        """
+        multipliziert zwei Brüche
+        :param other: anderer Bruch
+        :return: multiplizierter Bruch
+        """
         return Fraction(self.numerator * other.numerator, self.denominator * other.denominator)
 
     def __truediv__(self, other):
+        """
+        dividiert zwei Brüche
+        :param other: anderer Bruch
+        :return: dividierter Bruch
+        """
         return Fraction(self.numerator * other.denominator, self.denominator * other.numerator)
 
 
 if __name__ == "__main__":
     f1 = Fraction(1, 2)
-    f2 = Fraction(2, 3)
+    f2 = Fraction(-2, 3)
     print(f1)
     print(f2)
     print(f1 + f2)
